914b86888dd174d54d28c5d45e1f8ca6cfeef016
rissri <kasia.ptaszynska93@gmail.com>
2024-11-08 21:05:09 +0000

    API forecast update

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  
+  getForecast(response.data.city);
 }
 
 function searchCity(city) {
@@ -53,9 +55,16 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+  function getForecast(city) {
+let apiKey = "4tfe28oa94aa1643098d8fabd7caa6f0";
+let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  
+  axios(apiUrl).then(displayForecast);
 
-function displayForecast() {
- 
+}
+
+function displayForecast(response) {
+ console.log(response.data);
 
   let days = ["Tue","Wed","Thu","Fri","Sat"];
   let forecastHtml = "";
@@ -82,4 +91,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("London");
-displayForecast();
+
